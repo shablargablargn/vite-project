dffc2c02c2525682db93bca380e1caf9575389d1
Jonah <shablargablargn@New-PC.localdomain>
2023-09-05 13:40:10 -0500

    first map

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
+import {puppyList} from './data.js'
 import './App.css'
+import { useState } from 'react'
 
 function App() {
 
+const [puppies, setPuppies] = useState(puppyList)
+console.log("puppyList: ", puppyList);
   return (
-    <>
-      <div>
-
+      <div className="App">
+        { 
+          puppies.map((puppy) => {
+            return <p key={puppy.id}>{puppy.name}</p>
+          })
+        }
       </div>
-    </>
-  )
+  );
 }
 
 export default App
